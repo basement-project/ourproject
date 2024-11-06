d7d044abfb1ed2ce2e63313c7109262eb6edc75a
daffnr <daffanr20@gmail.com>
2024-11-06 21:32:07 +0700

    edit footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Hero from "./_component/Hero";
-import Footer from "./_component/Footer";
 
 
 const page = () => {
   return (
     <div>
       <Hero />
-      <Footer/>
     </div>
   );
 };
