7493b64945e87b73041c1f2c9bc8cf9b82683a3c
daffnr <daffanr20@gmail.com>
2024-12-02 21:48:09 +0700

    update footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-6">
+    <footer className="bg-gray-800 text-white py-6 mt-10">
     <div className="container mx-auto text-center">
       <div className="flex justify-center space-x-8 mb-4">
         {/* Social Media Links */}
